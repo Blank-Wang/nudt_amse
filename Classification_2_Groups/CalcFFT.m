2c5658e27a419df6c002cbf3e5bf2aa535db22e6
BLANK <wang_dezhi@hotmail.com>
2017-06-05 17:50:12 +0800

    Update CalcFFT.m

--- Classification_2_Groups/CalcFFT.m
+++ Classification_2_Groups/CalcFFT.m
@@ -11,7 +11,7 @@ FMatrix=zeros(noFrames-2, N/2+1); %Matrix to hold amplicate
 f = Fs/2*linspace(0,1,N/2+1);
 
 for i=1:noFrames-ceil(N/FrameStep)
-    frame=s((i-1)*FrameStep+1:(i-1)*FrameStep+N);  %每段数据为N个点，相邻两端数据起点间隔Framestep
+    frame=s((i-1)*FrameStep+1:(i-1)*FrameStep+N);  %Framestep
     F=frame.*hamming(N);                                         %multiplies each frame with hamming window
     FFTo=fft(F,N);                                                       %computes the fft
     oup=2*abs(FFTo(1:N/2+1));
